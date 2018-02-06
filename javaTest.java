427b192c4b61bb2ce3207abcc1c4592716937607
Shaik <masthanvali.shaik@manpowergroup.com>
2018-02-06 20:13:45 +0530

    javaTest.java

---
+++ javaTest.java
@@ -0,0 +1,14 @@
+public class javaTest{
+	public static void main(String args[]){
+
+		int a =10;
+		int b =10;
+
+		void add(int a, int b){
+			this.a = a;
+			this.b = b;
+			int c = a + b;
+			System.out.println("Result of A and B is ::: "+c);
+		}
+	}
+}
